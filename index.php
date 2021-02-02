8c03357de7b12dae356920b728c13e7f99c4bc1f
dakotawlacy <dwl0010@mix.wvu.edu>
2021-02-02 16:44:49 -0500

    Lab 2 About Page

---
+++ index.php
@@ -0,0 +1,57 @@
+<body>
+    <link rel="stylesheet" href="css/about.css">
+
+    <div class="about">
+        <h1>Teenage Mutant Pizza Reviews</h1>
+        <p>Since 1990, TMPR has mastered the craft of reviewing delicious pizzas in looks, taste, and structural integrity. Do you wish to get your pizza reviewed by our amazing team of shelled savants? Learn how you can apply for pizza review below by scheduling a FREE appointment today. </p>
+        
+    </div>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="images/1.jpeg">
+                <div class="container">
+                    <h2>Head Taste Tester</h2>
+                    <p class="c1">Michelangelo</p>
+                    <p class="c1">Email: pizzatime@cs230.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/2.jpeg">
+                <div class="container">
+                    <h2>Pizza Presentation Observationalist</h2>
+                    <p class="c2">Leonardo</p>
+                    <p class="c2">Email: turtlepower@cs230.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/3.jpeg">
+                <div class="container">
+                    <h2>CEO</h2>
+                    <p class="c3">Raphael</p>
+                    <p class="c3">Email: heroinhalfshell@cs230.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/4.jpeg">
+                <div class="container">
+                    <h2>Pizza Stress-Tester</h2>
+                    <p class="c4">Donatello</p>
+                    <p class="c4">Email: cowabunga@cs230.com</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="button">
+            <a href="https://www.youtube.com/watch?v=4co-d81mS7k" class="button">
+            APPLY NOW
+</a>
+    </div>
+
+</body>
